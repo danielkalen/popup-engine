f4fd3220501c210ccc543707c2137447db2f89a0
danielkalen <d@danielkalen.com>
2017-02-02 20:12:13 -0500

    [Build]

--- dist/popup-engine.debug.js
+++ dist/popup-engine.debug.js
@@ -138,7 +138,7 @@
     this.appendToDOM();
     return Popup.instances[this.name] = this;
   };
-  Popup.version = '3.0.1';
+  Popup.version = '3.0.2';
   Popup.instances = {};
   Popup.isOpen = false;
   Popup.defaultOptions = defaultOptions = {
